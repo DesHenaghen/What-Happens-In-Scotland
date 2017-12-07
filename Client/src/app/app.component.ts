69e4a0bca14b634f2cf84071fcde9828dfe4070d
Desmond Henaghen <desmond.henaghen@caterva.de>
2017-12-07 18:23:31 +0000

    Mademap interactive. Can now click on areas and area will be highlighted and display areas name

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 import * as d3 from 'd3';
-
-import * as t from 'topojson';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
-  title = 'Map Page';
-  subtitle = 'US Map';
+  ward = 'Glasgow';
+  private wards = {};
 
   private margin = {top: 20, right: 20, bottom: 0, left: 50};
   private height: number;
@@ -19,12 +18,12 @@ export class AppComponent implements OnInit {
   private svg: any;
   private g: any;
   private colour: any;
-  private projection: number;
+  private projection;
   private path: number;
 
   constructor() {
     this.width = 1000 - this.margin.left - this.margin.right;
-    this.height = 600 - this.margin.top - this.margin.bottom;
+    this.height = 800 - this.margin.top - this.margin.bottom;
   }
 
   ngOnInit() {
@@ -38,7 +37,7 @@ export class AppComponent implements OnInit {
       .center([-0.15, 55.8642])
       .rotate([4.1, 0])
       .parallels([50, 60])
-      .scale(170000)
+      .scale(250000)
       .translate([this.width / 2, this.height / 2]);
 
     this.colour = d3.scaleLinear()
@@ -55,48 +54,77 @@ export class AppComponent implements OnInit {
   }
 
   private drawMap() {
-    const localG = this.g;
-    const localPath = this.path;
-    const localColour = this.colour;
-    const localProjection: any = this.projection;
-    console.log(localProjection);
-    d3.json('./assets/json/glasgow-wards.json', function(error, topology) {
+    d3.json('./assets/json/glasgow-wards.json', (error, topology) => {
       if (error) {
         console.error(error);
       } else {
         console.log(topology);
-        // console.log(topology.features);
+
+        this.loadWards(topology);
 
         // Draw each ward polygon
-        localG.selectAll('path')
-          .data(topology.features) // Read in the array of features from the topology data
-          .enter()
-          .append('path') // Add a path element
-           // With classes wards and ward id
-           .attr('class', function (d) {
-             return 'wards ' + d.properties.WD13CD;
-           })
-          // Define the outline of the shape based on the defined projection and polygon shape
-          .attr('d', localPath)
-          // Fill the polygon in with a colour from a range
-          .attr('fill', function (d, i) {
-            return localColour(i);
-          });
+        this.drawWards(topology);
 
         // Place labels over each ward
-        localG.selectAll('.place-label')
-          .data(topology.features) // Read in the array of features from the topology data
-          .enter().append('text') // Add a text element
-          .attr('class', 'place-label')
-          // Start the text at the centre of the polygon
-          .attr('transform', function(d) {
-            return 'translate(' + localProjection(d3.polygonCentroid(d.geometry.coordinates[0])) + ')';
-          })
-          // Text size
-          .attr('dy', '.35em')
-          // Text value
-          .text(function(d) { return d.properties.WD13NM; });
+        // this.drawWardLabels(topology);
+
+        this.attachClickListeners(topology);
       }
     });
+  }
+
+  private loadWards(topology: any): void {
+    topology.features.forEach(feature => {
+        this.wards[feature.properties.WD13CD] = feature.properties.WD13NM;
+    });
+  }
+
+  private drawWards(topology: any): void {
+    this.g.selectAll('path')
+      .data(topology.features) // Read in the array of features from the topology data
+      .enter()
+      .append('path') // Add a path element
+      // With classes wards and ward id
+      .attr('class', d => 'wards ' + d.properties.WD13CD)
+      // Define the outline of the shape based on the defined projection and polygon shape
+      .attr('d', this.path)
+      // Fill the polygon in with a colour from a range
+      .attr('fill', (d, i) => this.colour(i))
+      .attr('id', d => d.properties.WD13CD);
+  }
+
+  private drawWardLabels(topology): void {
+    this.g.selectAll('.place-label')
+      .data(topology.features) // Read in the array of features from the topology data
+      .enter().append('text') // Add a text element
+      .attr('class', 'place-label')
+      // Start the text at the centre of the polygon
+      .attr('transform', d => 'translate(' + this.projection(d3.polygonCentroid(d.geometry.coordinates[0])) + ')')
+      // Text size
+      .attr('dy', '.35em')
+      // Text value
+      .text(d => d.properties.WD13NM);
+  }
+
+
+  private attachClickListeners(topology): void {
+    topology.features.forEach(f =>
+      document.getElementById(f.properties.WD13CD)
+        .addEventListener('click', this.setWards)
+    );
+    // document.getElementById('').addEventListener("click", function (f) {alert('xss')});
+  }
+
+  private setWards = (e) => {
+    const target: any = e.target;
+    this.ward = this.wards[target.id];
+    this.clearSelectedClass();
+    document.getElementById(target.id).classList.add('selected');
+  }
+
+  private clearSelectedClass = () => {
+    for (const [key] of Object.entries(this.wards)) {
+      document.getElementById(key).classList.remove('selected');
+    }
   }
 }
