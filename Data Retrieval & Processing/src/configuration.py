c77558df8692ed5a9edec224659966824deb987b
Desmond Henaghen <desmond.henaghen@geo-sport.de>
2018-01-23 10:29:57 +0000

    Scotland config

--- Data Retrieval & Processing/src/configuration.py
+++ Data Retrieval & Processing/src/configuration.py
@@ -41,15 +41,29 @@ class ProductionConfig(Config):
     CONSUMER_SECRET = '6UASMZoKUHI07SS6e6F9pXtGpODNZe8yoDNuwYfrzZU78AOX5G'
     ACCESS_TOKEN_KEY = '924952244293955584-CyeEIdTVybmwwpE6jH25KgciahTpz2S'
     ACCESS_TOKEN_SECRET = 'ggQSp6fdat3vGVe4MYnnvR4LBmQanYcMGhFW1E3lRrGnt'
-#
-#
-# if 'dev' in sys.argv:
-#     config = DevelopmentConfig()
-# else:
-#     config = ProductionConfig()
 
 
-config = DevelopmentConfig()
+class ScotlandConfig(Config):
+    DEBUG = False
+
+    PSQL_USERNAME = 'whig'
+    PSQL_PASSWORD = '382FkjBoQPfk'
+    PSQL_HOSTNAME = 'whathappensinglasgow.cszk7qzakguv.eu-west-2.rds.amazonaws.com'
+    PSQL_PORT = '5432'
+    PSQL_DATABASE = 'tweets'
+
+    CONSUMER_KEY = 'qUy64uUpmIeM2ARBPCxTIPFB6'
+    CONSUMER_SECRET = 'Hvshcc57G2hsnAV34xnyxTX7DrHS0TOoVTw4I38pCqsBEsHjDe'
+    ACCESS_TOKEN_KEY = '924952244293955584-fsZZrlCy331SOwfj37qzOeqVyOLg3k9'
+    ACCESS_TOKEN_SECRET = 'CpVNZJY73P89vu1SMeLplV0yI36X86HTKSwtaGQLTYwYO'
+
+
+if 'dev' in sys.argv:
+    config = DevelopmentConfig()
+elif 'scotland' in sys.argv:
+    config = ScotlandConfig()
+else:
+    config = ProductionConfig()
 
 
 def get_config():
