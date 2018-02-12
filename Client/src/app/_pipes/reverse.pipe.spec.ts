f2b1cbca8d059bb0f2a9bcbc70f7cfa10be2d8f8
Desmond Henaghen <desmond.henaghen@geo-sport.de>
2018-02-12 14:50:17 +0000

    Added pipe to GIT

---
+++ Client/src/app/_pipes/reverse.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ReversePipePipe } from './reverse.pipe';
+
+describe('ReversePipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ReversePipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
